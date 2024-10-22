9517f4212ffde0068c1d59d1001c18126c3faf14
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-10-22 16:59:07 -0300

    Add da Iaara e novo alinhamento de tela

--- Mitologias.java
+++ Mitologias.java
@@ -7,10 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Mitologias extends Application {
@@ -24,14 +23,18 @@ public class Mitologias extends Application {
         folclore.setStyle("-fx-background-image: url('file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/FlorestBackground.gif');" +
                 "-fx-background-size: cover;");
 
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(20));
-        vbox.setAlignment(Pos.CENTER);
+        GridPane grid = new GridPane();
+        grid.setHgap(20);
+        grid.setVgap(20);
+        grid.setPadding(new Insets(20));
+        grid.setAlignment(Pos.CENTER);
 
         Label tituloLabel = new Label("MITOLOGIAS BRASILEIRAS");
         tituloLabel.setId("titulo-label");
-        vbox.getChildren().add(tituloLabel);
+        folclore.setTop(tituloLabel);
+        BorderPane.setAlignment(tituloLabel, Pos.CENTER);
 
+        // Curupira
         Image curupiraImage = new Image("file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/Personagens/Curupira.png");
         ImageView curupiraImageView = new ImageView(curupiraImage);
         curupiraImageView.setFitWidth(100);
@@ -41,14 +44,15 @@ public class Mitologias extends Application {
         curupiraContainer.getChildren().add(curupiraImageView);
         curupiraContainer.setId("curupira-image");
         curupiraContainer.setPadding(new Insets(10));
-
         curupiraContainer.setOnMouseClicked(e -> {
             Stage curupiraStage = new Stage();
             Curipira curipira = new Curipira();
             curipira.start(curupiraStage);
             primaryStage.close();
         });
+        grid.add(curupiraContainer, 0, 0);
 
+        // Saci
         Image saciImage = new Image("file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/Personagens/Saci.png");
         ImageView saciImageView = new ImageView(saciImage);
         saciImageView.setFitWidth(100);
@@ -58,14 +62,15 @@ public class Mitologias extends Application {
         saciContainer.getChildren().add(saciImageView);
         saciContainer.setId("saci-image");
         saciContainer.setPadding(new Insets(10));
-
         saciContainer.setOnMouseClicked(e -> {
             Stage saciStage = new Stage();
             Saci saci = new Saci();
             saci.start(saciStage);
             primaryStage.close();
         });
+        grid.add(saciContainer, 1, 0);
 
+        // Boitata
         Image boitataImage = new Image("file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/Personagens/Boitata.gif");
         ImageView boitataImageView = new ImageView(boitataImage);
         boitataImageView.setFitWidth(100);
@@ -75,17 +80,33 @@ public class Mitologias extends Application {
         boitataContainer.getChildren().add(boitataImageView);
         boitataContainer.setId("boitata-image");
         boitataContainer.setPadding(new Insets(10));
-
         boitataContainer.setOnMouseClicked(e -> {
             Stage boitataStage = new Stage();
             Boitata boitata = new Boitata();
             boitata.start(boitataStage);
             primaryStage.close();
         });
+        grid.add(boitataContainer, 2, 0);
 
-        vbox.getChildren().addAll(curupiraContainer, saciContainer, boitataContainer);
+        // Iara
+        Image iaraImage = new Image("file:C:/Users/matheus.fgs/Desktop/FOLCLORE/Imagens/Personagens/Iara.png");
+        ImageView iaraImageView = new ImageView(iaraImage);
+        iaraImageView.setFitWidth(100);
+        iaraImageView.setFitHeight(100);
+        iaraImageView.setPreserveRatio(true);
+        StackPane iaraContainer = new StackPane();
+        iaraContainer.getChildren().add(iaraImageView);
+        iaraContainer.setId("iara-image");
+        iaraContainer.setPadding(new Insets(10));
+        iaraContainer.setOnMouseClicked(e -> {
+            Stage iaraStage = new Stage();
+            //Iara iara = new Iara();
+            //iara.start(iaraStage);
+            primaryStage.close();
+        });
+        grid.add(iaraContainer, 3, 0);
 
-        folclore.setCenter(vbox);
+        folclore.setCenter(grid);
 
         Button sair = new Button("SAIR");
         sair.setId("sair-button");
